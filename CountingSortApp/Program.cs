a8ef44f2a38e390dc9f3193cb5777e3571b2a2d1
Qoutel Qoutel <nezzy124@outlook.com>
2022-01-01 21:53:06 +0200

    add CountingSort class, change validation

--- CountingSortApp/Program.cs
+++ CountingSortApp/Program.cs
@@ -2,36 +2,19 @@
 
 namespace CountingSortApp
 {
-    class Program
+    public class CountingSort
     {
-        static bool LengthValidation (int arrayLength)
+        public List<int> GetFrequencyArray()
         {
-            if (arrayLength <= 0 || arrayLength > Math.Pow(10,6))
+            List<int> inputArray;
+            bool isValidate;
+            Console.WriteLine($"Enter space-separated array of 100+ integers in 1...99 range:");
+            do
             {
-                Console.WriteLine("Incorrect array length, try again:");
-                return false;
+                inputArray = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+                isValidate = ArrayValidation(inputArray);
             }
-            return true;
-        }
-        static bool ArrayValidation(List<int> inputArray, int arrayLength)
-        {
-            if (inputArray.Count != arrayLength)
-            {
-                Console.WriteLine($"The number of integers in array must be not less and not above than {arrayLength}, try again:");
-                return false;
-            }
-            for (int i = 0; i < inputArray.Count; i++)
-            {
-                if (inputArray[i] <= 0 || inputArray[i] >=100)
-                {
-                    Console.WriteLine($"{inputArray[i]} is incorrect value, enter correct values:");
-                    return false;
-                }
-            }
-            return true;
-        }
-        static List<int> GetFrequencyArray (List<int> inputArray)
-        {
+            while (!isValidate);
             List<int> frequencyArray = new List<int>();
             for (int i = 0; i < 100; i++)
             {
@@ -43,30 +26,34 @@ namespace CountingSortApp
             }
             return frequencyArray;
         }
+        static bool ArrayValidation(List<int> inputArray)
+        {
+            if (inputArray.Count <= 100 || inputArray.Count > Math.Pow(10, 6))
+            {
+                Console.WriteLine("Incorrect array length, try again:");
+                return false;
+            }
+            for (int i = 0; i < inputArray.Count; i++)
+            {
+                if (inputArray[i] <= 0 || inputArray[i] >= 100)
+                {
+                    Console.WriteLine($"{inputArray[i]} is incorrect value, enter correct values:");
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+    class Program
+    {
         static void Main(string[] args)
         {
-            int arrayLength;
-            bool isValidateLength;
-            bool isValidateArray;
-            List<int> inputArray;
-            Console.WriteLine("Enter array length:");
-            do
-            {
-                isValidateLength = Int32.TryParse(Console.ReadLine(), out arrayLength) && LengthValidation(arrayLength);
-            }
-            while (!isValidateLength);
-            Console.WriteLine($"Enter space-separated array of {arrayLength} integers in 1...99 range:");
-            do
-            {
-                inputArray = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
-                isValidateArray = ArrayValidation(inputArray, arrayLength);
-            }
-            while (!isValidateArray);
-            List<int> output = GetFrequencyArray(inputArray);
+            CountingSort frequencyArray = new CountingSort();
+            List <int> resultArray = frequencyArray.GetFrequencyArray();
             Console.WriteLine("Frequency array:");
-            foreach (int i in output)
+            foreach (int integer in resultArray)
             {
-                Console.Write(i + " ");
+                Console.Write(integer + " ");
             }
         }
     }
